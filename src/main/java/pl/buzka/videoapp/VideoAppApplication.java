5e9ae07dcc69fe3b12c27881d6fb8647f73e35aa
slaso <slaso@DESKTOP-MF5KUJA>
2021-09-24 13:12:22 +0200

    add swagger-ui

--- src/main/java/pl/buzka/videoapp/VideoAppApplication.java
+++ src/main/java/pl/buzka/videoapp/VideoAppApplication.java
@@ -2,8 +2,11 @@ package pl.buzka.videoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
 
 @SpringBootApplication
+@EnableOpenApi
 public class VideoAppApplication {
 
 	public static void main(String[] args) {
